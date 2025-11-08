356cc52d229d356e3510ca9b136504d0bf7f92fc
sajjad | سجاد <1380.9.15@gmail.com>
2025-11-08 15:36:07 +0330

    changed run_script.sh for randomly delay in running script

--- run_script.sh
+++ run_script.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sleep_time=$((RANDOM % 16 + 5))
+echo "Going to sleep for ${sleep_time} minutes..."
+sleep "${sleep_time}m"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
 ./behkad_linux
